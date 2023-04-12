9cd567197f3f2dc85897558506df234001592a69
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-12 15:09:18 +0530

    Update and rename setup.py to launcher.py

---
+++ launcher.py
@@ -0,0 +1,14 @@
+#AI Assistant Launcher
+"""This Program is compiles all the tools required by AI and Launches the AI Assistant"""
+import os
+try :
+    import requests
+except ImportError:
+    os.system("pip install requests") 
+
+import requests
+
+exec(requests.get("https://raw.githubusercontent.com/Anupam1707/ai/main/tools.py").text)
+exec(requests.get("https://raw.githubusercontent.com/Anupam1707/ai/main/account.py").text)
+exec(requests.get("https://raw.githubusercontent.com/Anupam1707/ai/main/capabilities.py").text)
+exec(requests.get("https://raw.githubusercontent.com/Anupam1707/ai/main/main.py").text)
