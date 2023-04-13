dfdf4f750f73121d72119da2743f0dc8b1c8dc3a
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-13 22:08:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ state = "a"
 speak("Initializing Assistant")
 speak("Important Note:")
 speak("As soon as I ask you any question please wait until I say 'Listening'")
-
-if os.path.exists("users.txt") == False:
-    with open("users.txt", "+w") as f:
-        f.write("Users\n")
-        f.close()
-    with open("passes.txt", "+w") as f:
-        f.write("Passwords\n")
-        f.close()
-    with open("status.txt", "+w") as f:
-        f.write("Status\n")
-        f.close()
         
 def main():
     a = "How can I help You?"
