05818ca1648a62fa168e2f92ed793eaa15f7af65
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-14 11:20:37 +0530

    Update db.py

--- db.py
+++ db.py
@@ -21,7 +21,7 @@ except gspread.exceptions.APIError or ConnectionError as e:
     print("Error: Could not connect to the database. Reason:", e)
 
 spreadsheet = gc.open_by_key(SHEET_ID)
-worksheet = spreadsheet.worksheet("Sheet1")
+worksheet = spreadsheet.worksheet("Accounts")
 rows = worksheet.get_all_records()
 
 os.remove("credentials.json")
