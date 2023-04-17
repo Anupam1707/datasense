5723302d983a87d0e90278825425e041edce7b1c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-17 21:15:33 +0530

    Update window.py

--- window.py
+++ window.py
@@ -36,13 +36,13 @@ def login_window():
     button.pack()
 
     login.mainloop()
+def graph_window():
+    l1val = l2val = l3val = ""
+    graph = Tk()
+    graph.geometry("1280x720")
+    graph.resizable(False,False)
 
-def home_window():
-    main = Tk()
-    main.geometry("1280x720")
-    main.resizable(False,False)
-
-    response = requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/bg.jpg")
+    response = requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/graph/bg.jpg")
     img = Image.open(BytesIO(response.content))
     img = img.resize((1280,720), Image.LANCZOS)
     test = ImageTk.PhotoImage(img)
@@ -50,34 +50,19 @@ def home_window():
     bk.image = test
     bk.place(x=0, y=0)
 
-    main.title("Food Sales Management")
-    title = Label(main, text= 'Food Sales Management', font= 'Arial 35 bold',bg='#7676EE').pack(pady=0)
-    Button(main, text = 'Exit', font = 'Arial 20 bold', bg='red', command=main.destroy).pack(side = BOTTOM,anchor = "se")
-
-    # Create the first dropdown menu
-    option_1_label = tk.Label(main, text="Option 1")
-    option_1_label.grid(row=0, column=0)
-    option_1_var = tk.StringVar()
-    option_1_dropdown = ttk.Combobox(main, width=15, textvariable=option_1_var, state="readonly")
-    option_1_dropdown["values"] = ("Option A", "Option B", "Option C")
-    option_1_dropdown.grid(row=0, column=1)
-
-    # Create the second dropdown menu
-    option_2_label = tk.Label(main, text="Option 2")
-    option_2_label.grid(row=1, column=0)
-    option_2_var = tk.StringVar()
-    option_2_dropdown = ttk.Combobox(main, width=15, textvariable=option_2_var, state="readonly")
-    option_2_dropdown["values"] = ("Option D", "Option E", "Option F")
-    option_2_dropdown.grid(row=1, column=1)
-
-    # Create the third dropdown menu
-    option_3_label = tk.Label(main, text="Option 3")
-    option_3_label.grid(row=2, column=0)
-    option_3_var = tk.StringVar()
-    option_3_dropdown = ttk.Combobox(main, width=15, textvariable=option_3_var, state="readonly")
-    option_3_dropdown["values"] = ("Option G", "Option H", "Option I")
-    option_3_dropdown.grid(row=2, column=1)
-
-    main.mainloop()
+    graph.title("Food Sales Management")
+    title = Label(graph, text= 'Food Sales Management', font= 'Arial 35 bold',bg='#7676EE').pack()
+    l1 = Label(graph, text="Data List 1", font= "Arial 30", fg = "black").place(y = 50)
+    l1inp = Entry(graph, textvariable= l1val, width = 24, font='Arial 26 bold').place(y = 100)
+    l2 = Label(graph, text="Data List 2", font= "Arial 30", fg = "black").pack(pady = 0)
+    l2inp = Entry(graph, textvariable= l2val, width = 24, font='Arial 26 bold').pack(pady= 0)
+    l3 = Label(graph, text="Data List 2", font= "Arial 30", fg = "black").pack(pady=0)
+    l3inp = Entry(graph, textvariable= l3val, width = 24, font='Arial 26 bold').pack(pady=0)
+    
+    Button(graph, text = 'Exit', font = 'Arial 20 bold', bg='red', command=graph.destroy).pack(side = BOTTOM,anchor = "se")
+    
+    
+        
+    graph.graphloop()
     
 login_window()
