300f6c9dc2d2c09a82c131b41e8c1b442868ebfc
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-17 21:37:31 +0530

    Update aicap.py

--- aicap.py
+++ aicap.py
@@ -1,12 +1,5 @@
 def caps():
-    print("Choose between :")
-    print("1. Keyboard")
-    print("2. Voice")
-    q = int(input("Enter on of the number : "))
-    if q == 1:
-        query = str(input("Enter your query : "))
-    elif q == 2:
-        query = takeCommand()
+    query = takeCommand()
     if "plot a" in query:
         query = query[6:]
         if "horizontal bar graph" in query:
