5702a199acb187c58b4e601a89d4989dde37b20f
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-17 21:36:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import os
 
-state = "a"
-
 speak("Initializing Assistant")
 speak("Important Note:")
 speak("As soon as I ask you any question please wait until I say 'Listening'")
         
 def main():
-    a = "How can I help You?"
-    b = "Do you have any other Query or Question"
-    state = a
-
-    speak(state)
+    speak("How can I help You?")
     caps()
 
 main()
