3cb57426c6c1bc6ff776bf82f2beafdd56c3bb90
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-17 22:22:16 +0530

    Update window.py

--- window.py
+++ window.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+def fetch(l1val = None, l2val = None, l3val = None, t):
+    if l1val != None and l2val != None and l3val == None and t == "bv":
+        plotb(data(sales, col[l1val.lower()]), data(sales, col[l2val.lower()]), t = "bv")
+
 def login_window():
     login = Tk()
     login.title("Login")
@@ -27,7 +31,7 @@ def login_window():
                         usr = -1
         if password_entry.get() == rows[usr]["Password"]:
             login.destroy() 
-            graph_window()
+            home_window()
         else:
             error_label = Label(login, text="Incorrect username or password", fg="red")
             error_label.pack()
@@ -37,7 +41,10 @@ def login_window():
 
     login.mainloop()
 def graph_window():
-    l1val = l2val = l3val = ""
+    l1val = None
+    l2val = None
+    l3val = None
+    typ = ""
     graph = Tk()
     graph.geometry("1280x720")
     graph.resizable(False,False)
@@ -52,17 +59,17 @@ def graph_window():
 
     graph.title("Food Sales Management")
     title = Label(graph, text= 'Food Sales Management', font= 'Arial 35 bold',bg='#7676EE').pack()
-    l1 = Label(graph, text="Data List 1", font= "Arial 30", fg = "black").place(y = 50)
-    l1inp = Entry(graph, textvariable= l1val, width = 24, font='Arial 26 bold').place(y = 100)
-    l2 = Label(graph, text="Data List 2", font= "Arial 30", fg = "black").pack(pady = 0)
-    l2inp = Entry(graph, textvariable= l2val, width = 24, font='Arial 26 bold').pack(pady= 0)
-    l3 = Label(graph, text="Data List 2", font= "Arial 30", fg = "black").pack(pady=0)
-    l3inp = Entry(graph, textvariable= l3val, width = 24, font='Arial 26 bold').pack(pady=0)
+    l1 = Label(graph, text="Data List 1", font= "Arial 30", fg = "black").pack()
+    l1inp = Entry(graph, textvariable= l1val, width = 24, font='Arial 26 bold').pack()
+    l2 = Label(graph, text="Data List 2", font= "Arial 30", fg = "black").pack()
+    l2inp = Entry(graph, textvariable= l2val, width = 24, font='Arial 26 bold').pack()
+    l3 = Label(graph, text="Data List 3", font= "Arial 30", fg = "black").pack()
+    l3inp = Entry(graph, textvariable= l3val, width = 24, font='Arial 26 bold').pack()
+    l4 = Label(graph, text="Type of Graph", font= "Arial 30", fg = "black").pack()
+    l4inp = Entry(graph, textvariable= typ, width = 24, font='Arial 26 bold').pack()
+    Button(graph, text= "Plot", font = "Arial 20 bold", bg="skyblue", command=fetch(l1val, l2val, l3val, typ)).pack()
     
-    Button(graph, text = 'Exit', font = 'Arial 20 bold', bg='red', command=graph.destroy).pack(side = BOTTOM,anchor = "se")
-    
-    
-        
-    graph.graphloop()
+    Button(graph, text = 'Exit', font = 'Arial 20 bold', bg='red', command=graph.destroy).pack(side = BOTTOM,anchor = "se")    
+    graph.mainloop()
     
 login_window()
