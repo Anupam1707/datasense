4e13a8214a8b03ad7b750821c7beaf13532be396
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-18 20:51:05 +0530

    Update window.py

--- window.py
+++ window.py
@@ -86,4 +86,4 @@ def graph_window():
     Button(graph, text = 'Exit', font = 'Arial 20 bold', bg='red', command=graph.destroy).pack(side = BOTTOM,anchor = "se")    
     graph.mainloop()
     
-graph_window()
+login_window()
