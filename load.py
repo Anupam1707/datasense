3fc256f745acdfce0e5e09ac6764aaa00cf43a8e
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-22 08:32:23 +0530

    Update load.py

--- load.py
+++ load.py
@@ -1,19 +1,19 @@
-##from tkinter import *
-##def load():
-##    from tkinter import ttk
-##
-##    def start_loading():
-##
-##            message_label = Label(load, text="Loading, please wait...")
-##            message_label.pack(pady=10)
-##
-##            progress_bar = ttk.Progressbar(load, length=300, mode='indeterminate')
-##            progress_bar.pack(pady=10)
-##
-##            progress_bar.start()
-##    start_loading()
-##    
-##load = Tk()
-##load.title("Loading")
-##load.mainloop()
-##load()
+from tkinter import *
+def load():
+    from tkinter import ttk
+
+    def start_loading():
+
+            message_label = Label(load, text="Loading, please wait...")
+            message_label.pack(pady=10)
+
+            progress_bar = ttk.Progressbar(load, length=300, mode='indeterminate')
+            progress_bar.pack(pady=10)
+
+            progress_bar.start()
+    start_loading()
+    
+load = Tk()
+load.title("Loading")
+load.mainloop()
+load()
