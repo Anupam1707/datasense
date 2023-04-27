8e1756ed2e34168261393dd10c47f6a86a330e7c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-04-27 23:40:32 +0530

    Add files via upload

---
+++ length.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/"
+urls = ["db.py","visuals.py","window.py", "launcher.py"]
+
+sum = 0
+
+d = []
+
+for i in urls:
+    d = (requests.get(url + i)).text
+    with open("len.txt","w", newline = "") as f:
+        f.write(d)
+    with open("len.txt","r") as f:
+        sum += len(f.readlines())
+
+    
