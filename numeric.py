44e2283ee2ed97212ae7472bf0021d20a9d207fc
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-03 22:32:18 +0530

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+# Declare global variables
+citytren = None
+countrytren = None
+regiontren = None
+backtren = None
+
+def numeric_window():
+    global citytren, countrytren, regiontren, backtren  # Use global keyword to access global variables
+    numeric = Tk()
+    numeric.title("Numeric Analysis")
+    numeric.geometry("1280x720")
+
+    def trend():
+        tren.pack_forget()
+        
+        citytren.pack()
+        countrytren.pack()
+        regiontren.pack()
+        backtren.pack()
+        
+    def untrend():
+        tren.pack()
+        
+        citytren.pack_forget()
+        countrytren.pack_forget()
+        regiontren.pack_forget()
+        backtren.pack_forget()
+
+    citytren = Button(numeric, text= "City-Wise", font = "Arial 20 bold", bg="skyblue", command=trend)
+    countrytren = Button(numeric, text= "Country-Wise", font = "Arial 20 bold", bg="skyblue", command=trend)
+    regiontren = Button(numeric, text= "Region-Wise", font = "Arial 20 bold", bg="skyblue", command=trend)
+    backtren = Button(numeric, text= "Back", font = "Arial 20 bold", bg="skyblue", command=untrend)
+    
+    response = requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/bg.jpg")
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((1280,720), Image.LANCZOS)
+    test = ImageTk.PhotoImage(img)
+    bk = Label(image=test)
+    bk.image = test
+    bk.place(x=0, y=0)
+
+    
+    Label(numeric, text= 'Numeric Analysis', font= 'Arial 35 bold',bg='#7676EE').pack(pady = 10)
+    tren = Button(numeric, text= "Trending", font = "Arial 20 bold", bg="skyblue", command=trend)
+    tren.pack()
+    
+    numeric.mainloop()
+
+numeric_window()
