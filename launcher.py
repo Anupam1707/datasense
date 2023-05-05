cd28bad9b4701a41072826476d852015caec2a66
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-05 23:54:51 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,78 +1,57 @@
-#Food Analysis App Launcher
-"""This Program compiles all the tools required by Food Analysis App and launches the Login Page"""
-
-# Import all libraries required by the app here
-from io import BytesIO
+from tkinter import *
+from PIL import Image, ImageTk
 import os
 import requests
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
-import numpy as np
-import matplotlib.pyplot as plt
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-# Install libraries if not found
-try:
-    import requests
-    from tkinter import *
-    from PIL import Image, ImageTk
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import gspread
-    from oauth2client.service_account import ServiceAccountCredentials
-except ImportError:
-    os.system("pip install requests pillow numpy matplotlib gspread oauth2client")
-
-# Define the function that loads and executes the required modules
-def call():
-    # Create a list of the module URLs to load
-    module_urls = [
-        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py",
-        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py",
-        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/numeric.py",
-        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/window.py"
-    ]
-
-    # Set the maximum progress value based on the number of modules to load
-    max_progress = len(module_urls)
-
-    # Initialize the progress value to zero
-    progress = 0
-    progress_bar.config(maximum=max_progress, value=progress)
-
-    # Load each module and increment the progress value
-    for module_url in module_urls:
-        exec(requests.get(module_url).text)
-        progress += 1
-        progress_bar.config(value=progress)
-        load.update_idletasks()
-
-    # Stop the progress bar and destroy the window
-    progress_bar.stop()
-    load.destroy()
-
-    # Call the function to launch the login window
-    login_window()
+from tkinter.ttk import Progressbar
 
 # Create the main window
-load = Tk()
-load.title("Loading")
+root = Tk()
 
-# Add a label to show the loading message
-message_label = Label(load, text="Loading, please wait...")
-message_label.pack(pady=10)
+# Set the window properties
+root.overrideredirect(True)  # Removes the title bar and borders
+root.wm_attributes("-topmost", True)  # Keep the window on top
+root.wm_attributes("-alpha", 0.7)  # Set the window transparency
 
-# Add a progress bar to show the loading progress
-progress_bar = ttk.Progressbar(load, length=300, mode='determinate')
-progress_bar.pack(pady=10)
+# Load and display the logo image
+logo_image = ImageTk.PhotoImage(Image.open("splash.png"))
+logo_label = Label(root, image=logo_image)
+logo_label.pack(pady=50)
 
-# Start the progress bar
-progress_bar.start()
+# Add a loading spinner
+spinner_label = Label(root, text="Loading...", font=("Arial", 12))
+spinner_label.pack(pady=10)
+spinner = Progressbar(root, mode="indeterminate")
+spinner.pack(pady=10)
+spinner.start()
 
-# Call the function that will stop the progress bar and destroy the window after it has finished executing
-load.after(0, call)
+# Position the window at the center of the screen
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+x = (width // 2) - (logo_image.width() // 2)
+y = (height // 2) - (logo_image.height() // 2)
+root.geometry("+{}+{}".format(x, y))
+
+# Create a list of the module URLs to load
+module_urls = [
+    "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py",
+    "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py",
+    "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/numeric.py",
+    "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/window.py"
+]
+
+# Load each module
+for i, module_url in enumerate(module_urls):
+    exec(requests.get(module_url).text)
+
+# Stop the loading spinner
+spinner.stop()
+spinner_label.destroy()
+
+# Destroy the translucent window
+root.destroy()
+
+# Call the function to display the login window after a 1-second delay
+root.after(1000, login_window)
 
 # Run the main event loop
-load.mainloop()
+mainloop()
