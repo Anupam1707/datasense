ec7b4d440daa99f8f9ac7b696b8e98a7ec92380b
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-05 15:37:14 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,47 +1,78 @@
 #Food Analysis App Launcher
-"""This Program is compiles all the tools required by Food Analysis App and Launches the Login Page"""
+"""This Program compiles all the tools required by Food Analysis App and launches the Login Page"""
 
-#Import all libraries required by the app here
+# Import all libraries required by the app here
 from io import BytesIO
-import time
 import os
-try :
+import requests
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+import numpy as np
+import matplotlib.pyplot as plt
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Install libraries if not found
+try:
     import requests
     from tkinter import *
     from PIL import Image, ImageTk
     import numpy as np
     import matplotlib.pyplot as plt
-    import requests
     import gspread
     from oauth2client.service_account import ServiceAccountCredentials
-
-#Install libraries if not found
 except ImportError:
-    os.system("pip install requests tkinter pillow numpy matplotlib gspread oauth2client") 
+    os.system("pip install requests pillow numpy matplotlib gspread oauth2client")
 
-import requests
-from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
-
-#Function to compile all other files
+# Define the function that loads and executes the required modules
 def call():
-    exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py").text)
-    exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py").text)
-    exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/numeric.py").text)
-    exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/window.py").text)
+    # Create a list of the module URLs to load
+    module_urls = [
+        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py",
+        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py",
+        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/numeric.py",
+        "https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/window.py"
+    ]
+
+    # Set the maximum progress value based on the number of modules to load
+    max_progress = len(module_urls)
+
+    # Initialize the progress value to zero
+    progress = 0
+    progress_bar.config(maximum=max_progress, value=progress)
+
+    # Load each module and increment the progress value
+    for module_url in module_urls:
+        exec(requests.get(module_url).text)
+        progress += 1
+        progress_bar.config(value=progress)
+        load.update_idletasks()
+
+    # Stop the progress bar and destroy the window
     progress_bar.stop()
     load.destroy()
+
+    # Call the function to launch the login window
     login_window()
 
-#Creating a Loading Popup to engage the user while compiling all other files
-def load():
-    load = Tk()
-    message_label = Label(load, text="Loading, please wait...")
-    message_label.pack(pady=10)
-    progress_bar = ttk.Progressbar(load, length=300, mode='indeterminate')
-    progress_bar.pack(pady=10)
-    progress_bar.start()
-    load.title("Loading")
-    call()
-load()
+# Create the main window
+load = Tk()
+load.title("Loading")
+
+# Add a label to show the loading message
+message_label = Label(load, text="Loading, please wait...")
+message_label.pack(pady=10)
+
+# Add a progress bar to show the loading progress
+progress_bar = ttk.Progressbar(load, length=300, mode='determinate')
+progress_bar.pack(pady=10)
+
+# Start the progress bar
+progress_bar.start()
+
+# Call the function that will stop the progress bar and destroy the window after it has finished executing
+load.after(0, call)
+
+# Run the main event loop
+load.mainloop()
