4c1a65de52d9d467c71c360cf9ed7e701c9188e9
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-08 15:39:15 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,25 +1,7 @@
 #AI Assistant Launcher
 """This Program is compiles all the tools required by AI and Launches the AI Assistant"""
 
-from io import BytesIO
-import time
-import os
-try :
-    import requests
-    from tkinter import *
-    from PIL import Image, ImageTk
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import requests
-    import gspread
-    from oauth2client.service_account import ServiceAccountCredentials
-
-except ImportError:
-    os.system("pip install requests tkinter pillow numpy matplotlib gspread oauth2client") 
-
 import requests
-from tkinter import *
-from PIL import Image, ImageTk
 
 exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py").text)
 exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py").text)
