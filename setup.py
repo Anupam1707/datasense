5f664cb9830cfe78a6c6ef1680d14767ae61bc4c
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-21 21:44:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 """This file helps you to setup ypur app accoriding to your entries"""
 import time
+import os
+
+try :
+  from SecuriPy import *
+except ImportError:
+  os.system("pip install SecuriPy")
 
 print("Welcome to the DataBase Analysis App")
 time.sleep(2)
@@ -8,4 +14,11 @@ time.sleep(1)
 
 print("First of all add an editor in google sheets to this email:\ndatabase@database--387415.iam.gserviceaccount.com\nand confirm by typing yes")
 chk = str(input())
-SHEET_ID = str(input("Enter the link of Google Sheet : "))
+if chk == "Yes" or chk == "yes" or chk == "y" or chk == "Y":
+  SHEET_ID = str(input("Enter the link of Google Sheet : "))
+  
+SHEET_ID = SHEET_ID[39:]
+SHEET_ID = encrypt(SHEET_ID, "database")
+with open("data", "w") as d:
+  d.write(f"SHEET : {SHEET_ID}")
+  
