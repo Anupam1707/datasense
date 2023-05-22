89ffde85b089f0edac7efe99ae74b1d9d54d82c5
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-22 12:46:02 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,8 +1,12 @@
 #AI Assistant Launcher
 """This Program is compiles all the tools required by AI and Launches the AI Assistant"""
 
-import requests
-
+try:
+  import requests
+except ImportError:
+  import os
+  os.system("pip install requests")
+  
 exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/db.py").text)
 exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/visuals.py").text)
 exec(requests.get("https://raw.githubusercontent.com/Anupam1707/food-sales-analysis/main/numeric.py").text)
