60e15acaf7eb415d5b5ef6215c86f6bf9b233401
Anupam Kanoongo <96910096+Anupam1707@users.noreply.github.com>
2023-05-25 09:41:18 +0530

    Create write.py

---
+++ write.py
@@ -0,0 +1,10 @@
+from fetchify import *
+files = ["db.py","visuals.py", "numeric.py", "window.py"]
+
+with open("main.py","w") as f:
+    pass
+
+for file in files:
+    with open("main.py","a",encoding = "utf-8" ,newline = "") as main:
+        main.write(sense(file))
+        main.write("\n###############################################\n\n")
